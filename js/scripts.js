d055a8f754dc96fee9fbcfe85430e1fa35a84a6d
Deya Card <thedeyacard@gmail.com>
2022-02-11 15:26:00 -0800

    further test funtion 1-4

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,21 @@
 
 // Business Logic
 
-
 function mrRobogers(number) {
   let numberArr = []
 
   for (let i = 0; i <= number; i++) {
-    if (i.toString().includes("3")) {
-      numberArr.push("Won't you be my neighbor?");
-    } 
-    
-    
-    else if (i.toString().includes("2")) {
-      numberArr.push("Boop!");
-    } else if (i.toString().includes("1")) {
-      numberArr.push("Beep!");
-    }else {
-      numberArr.push(index);
-    }
-  }
-  return numberArr
-}
-
-
-
-
+   
 
 
 
 // UI Logic
-
-
 
 $(document).ready(function() {
   $("form#number-counter").submit(function() {
     event.preventDefault();
     const passage = $("#text-passage").val();
     const number = $("#number").val();
-    const numberCount = numberCounter(passage);
-    const occurrencesOfNumber = numberOfOccurrencesInText(number, passage);
     $("#special-message").html(numberCount);
-    $("#selected-count").html(occurrencesOfNumber);
-    $("#bolded-passage").html(boldPassage(number, passage));
   });
 });
