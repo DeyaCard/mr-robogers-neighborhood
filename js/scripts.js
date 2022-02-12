18aaf8aa94657a81934b4995581c84ffab216814
Deya Card <thedeyacard@gmail.com>
2022-02-12 12:00:49 -0800

    work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ function mrRobogers(number) {
   let numberArr = []
 
   for (let i = 0; i <= number; i++) {
-    if (i.toString().includes("3")) {
+    if (i.mrString().includes("3")) {
       numberArr.push("Won't you be my neighbor?");
-    } else if (i.toString().includes("2")) {
+    } else if (i.mrString().includes("2")) {
       numberArr.push("Boop!");
-    } else if (i.toString().includes("1")) {
+    } else if (i.mrString().includes("1")) {
       numberArr.push("Beep!");
     } else {
       numberArr.push(i);
@@ -17,7 +17,7 @@ function mrRobogers(number) {
   }
   return numberArr;  
 }
-console.log
+
    
 
 
@@ -28,7 +28,7 @@ $(document).ready(function() {
     event.preventDefault();
     const passage = $("#number-passage").val();
     const number = $("#number").val();
-    
+
     $(".special-message").text(number);
   });
 });
